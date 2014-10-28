3ea97c11e4bdce0902c1fc73abe34ffa34853756
Dmitry Kalinin <null.none@yandex.ru>
2014-10-28 20:46:11 +0200

    improved code

--- backend/script.py
+++ backend/script.py
@@ -22,10 +22,7 @@ cors = CORS(app)
 def init():
 	cursor = mysql.connect().cursor()
 	cursor.execute("SELECT `job_title` FROM `rank_candidate` WHERE `job_title`<>' ' GROUP BY `job_title` ")
-	job_title_list = cursor.fetchall()
-	job_title = []
-	for item in job_title_list:
-		job_title.append(item)
+	job_title = list(cursor.fetchall())
 	return json.dumps({"job_title": job_title})
 
 
@@ -43,32 +40,22 @@ def search():
 	cursor.execute("SELECT COUNT(*) FROM rank_candidate WHERE `salary_min` BETWEEN 0 AND {0} AND rank_candidate.job_title='{1}'".format(salary, special))
 	candidate_count = cursor.fetchone()[0]
 	sum_jobs_procent_deals = 0.0
-	sum_jobs = 0
-	count_recruiter = 0
-	for item in recruiter_jobs:
-		count_recruiter += 1
-		sum_jobs += item[0]
+	sum_jobs = sum([item[0] for item in recruiter_jobs])
 	
-	try:
+	if candidate_count:
 		sum_jobs_procent_deals = float(sum_jobs / candidate_count)
-	except Exception, e:
+	else:
 		sum_jobs_procent_deals = 0
 
-	stats_candidate_deals = [count_recruiter, sum_jobs_procent_deals]
+	stats_candidate_deals = [candidate_count, sum_jobs_procent_deals]
 
 	# ТОП наиболее активных работодателей на Джинне
 	cursor.execute("SELECT `recruiter_id`,rank_recruiter.*, COUNT(*) AS recruiter FROM rank_poke LEFT JOIN rank_recruiter ON rank_recruiter.id=rank_poke.recruiter_id GROUP BY `recruiter_id` ORDER BY recruiter DESC LIMIT 3")
-	stats_top_list = cursor.fetchall()
-	stats_top = []
-	for item in stats_top_list:
-		stats_top.append(item)
+	stats_top = list(cursor.fetchall())
 
 	# Примеры самых успешных профилей на Джинне
 	cursor.execute("SELECT * FROM `rank_recruiter` ORDER BY RAND() LIMIT 0,5")
-	stats_popular_list = cursor.fetchall()
-	stats_popular = []
-	for item in stats_popular_list:
-		stats_popular.append(item)
+	stats_popular = list(cursor.fetchall())
 
 	return json.dumps({"stats_popular": stats_popular, "stats_top": stats_top, "stats_candidate_deals": stats_candidate_deals})
 
